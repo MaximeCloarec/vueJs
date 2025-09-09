6cdc0ec0fced246eb9a538105a2d913371641550
MaximeCloarec <maxime.cloarec@yahoo.fr>
2025-09-09 15:28:08 +0200

    First init

---
+++ src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>HOME</h1>
+    </div>
+</template>
+
+<script setup="js"></script>
+
+<style></style>
---
+++ src/pages/exercice/exerciceInterPolation.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>{{ text }}</p>
+    <button @click="random()" class="btn">Reload</button>
+</template>
+
+<script setup lang="js">
+
+import { ref } from 'vue'
+
+let text = ref("")
+
+function random(){
+    if(Math.random()>0.5){
+        text.value = "livre boba fett"
+    }else{
+        text.value = "livre de la jungle"
+    }
+}
+</script>
+
+<style scoped></style>
